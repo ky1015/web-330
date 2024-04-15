be947f2869cd8fa453bb39d2bbc0185ed85ab832
ky1015 <kylie.struhs@gmail.com>
2024-04-14 19:42:57 -0600

    added week 4 work

---
+++ week-4/project10-01/project10-01.js
@@ -0,0 +1,86 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 10
+      Project 10-01
+
+      Project to create a drag and drop jigsaw puzzle
+      Author: Kylie Struhs
+      Date:   April 14 2024
+
+      Filename: project10-01.js
+*/
+
+// Reference to the puzzle board
+let puzzleBoard = document.getElementById("puzzleBoard");
+// Counter for the zIndex style of each puzzle piece
+let zCounter = 1;
+// Array of integers from 1 to 48
+let intList = new Array(48);
+// pointerX and pointerY will contain the initial coordinates of the pointerX
+// pieceX and pieceY will contain the initial coordinates of a puzzle piece
+let pointerX, pointerY, pieceX, pieceY;
+
+// Sort the integers from 1 to 48 in random order
+for (let i = 0; i < 48 ; i++) {
+   intList[i] = i+1;
+}
+intList.sort(function() {
+   return 0.5 - Math.random();
+});
+
+// generate randomly-sorted puzzle pieces
+for (let i = 0; i < 48; i++) {
+   let piece = document.createElement("img");
+   piece.src = "piece" + intList[i] + ".png";
+   let rowNum = Math.ceil((i+1)/8);
+   let colNum = (i + 1) - (rowNum - 1)*8;
+   piece.style.top = (rowNum - 1)*98 + 7 + "px";
+   piece.style.left = (colNum - 1)*98 + 7 + "px";
+   piece.draggable = false; // override the default draggability of images
+   puzzleBoard.appendChild(piece);
+}
+
+// Node list representing the puzzle pieces
+let pieces = document.querySelectorAll("div#puzzleBoard img");
+//console.log("Pieces= " + pieces)
+//console.log("Pieces.length= " + pieces.length)
+
+// For loop that iterates through every item in the pieces node list and adds an event listener that runs the grabPiece function in response to a pointerdown event
+for (let i = 0; i < pieces.length; i++) {
+  pieces[i].addEventListener("pointerdown", grabPiece);
+}
+
+function grabPiece(e) {
+pointerX = e.clientX;
+pointerY = e.clientY;
+console.log("Mouse is at " + pointerX + "," +pointerY)
+e.target.style.touchAction = "none";
+zCounter ++;
+e.target.style.zIndex = zCounter;
+
+// store initial values of piece
+pieceX = e.target.offsetLeft;
+pieceY = e.target.offsetTop;
+// add event listeners to the pieces
+e.target.addEventListener("pointermove", movePiece);
+e.target.addEventListener("pointerup", dropPiece);
+}
+
+function movePiece(e) {
+  let currentX = e.clientX
+  let currentY = e.clientY
+  let diffX = currentX - pointerX;
+  let diffY = currentY - pointerY;
+  console.log("current x,y" + currentX + "," + currentY)
+  console.log("diff x,y" + diffX + "," + diffY)
+
+
+  // calculate piece's new position
+  e.target.style.left = pieceX + diffX + "px";
+  e.target.style.top = pieceY + diffY + "px";
+}
+
+function dropPiece(e) {
+  e.target.removeEventListener("pointermove", movePiece);
+  e.target.removeEventListener("pointerup", dropPiece)
+}
